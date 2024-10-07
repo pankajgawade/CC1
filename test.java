fc15d97c9a1b402de53530f962c47a8dfed42663
Pankaj Gawade <135791801+pankajgawade@users.noreply.github.com>
2024-10-07 10:01:04 +0530

    Update test.java

--- test.java
+++ test.java
@@ -1,5 +1,5 @@
 class test{
   public static void main(String args[]){
-      System.out.println("This is ");
+      System.out.println("This is a Program from my github account and executing it using google cloud console ");
   }
 }
