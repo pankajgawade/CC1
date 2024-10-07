8eeafaaeb0e98925e33ca3099cb6ffc78c7543cd
Pankaj Gawade <135791801+pankajgawade@users.noreply.github.com>
2024-10-07 10:06:30 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,1 @@
-printf("This is a Program from my github account and executing it using google cloud console")
+print("This is a Program from my github account and executing it using google cloud console")
